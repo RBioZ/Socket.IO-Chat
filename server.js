658cee73f5d3b40bc2845d5b9bb81e84b4767e13
Jonatha Rihan <jonatha_rihan@hotmail.com>
2020-01-28 16:42:50 +0000

    Novas alterações

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
 
 app.get('/',(req,res)=>{
 	res.sendFile(__dirname+'/index.html')
 });
 
-app.get('/chat',(req,res)=>{
-	res.sendFile(__dirname+'/chat.html')
+app.post('/chat/',(req,res)=>{
+	res.sendFile(__dirname+'/chat.html');
+	//console.log(req.body.nickname);
 });
 
 
 const users = {}
+
 
 io.on('connection',(socket)=>{
 
@@ -22,10 +30,13 @@ io.on('connection',(socket)=>{
 	})
 	socket.on('login',function(data){
 		console.log(data.UserId+' connected')
-
 	})
 	socket.on('disconnect', function(){
 		console.log('user '+users[socket.id]+' disconected')
+	})
+	socket.on('chat', function(id){
+		users[id] = req.body.nickname
+		console.log(req.body.nickname)
 	})
 })
 
